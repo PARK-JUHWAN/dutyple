85a7a7a96e5e3bfa6739c0e23d1c16bc9f47e599
PARK-JUHWAN <naruto0414@gmail.com>
2025-05-26 10:58:38 +0900

    Fix: bind Flask to 0.0.0.0 for Render deployment

--- dutyple_api.py
+++ dutyple_api.py
@@ -55,4 +55,4 @@ def get_result(uid):
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
