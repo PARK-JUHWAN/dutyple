a4bf6c588606bf441258fcb0466eaef8993179c9
PARK-JUHWAN <naruto0414@gmail.com>
2025-05-28 11:27:35 +0900

    Deploy latest Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2509993946"
+    serviceWorkerVersion: "1797037753"
   }
 });
